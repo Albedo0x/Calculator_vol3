4e22ccbfcdf9e630189f5c43e3f5c8f9ff6d5391
Albedo0x <nikita.v.katyshev@gmail.com>
2022-11-25 18:40:52 +0300

    Calculator based on React, added less

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="calculator">
+          <div className="calculator-screen">
+            <div className="calculator-screen__field">
+              <p id="screen"></p>
+            </div>
+          </div>
+          <div className="calculator-input" id="main-input">
+            <div className="calculator-btn btn-pull">MC</div>
+            <div className="calculator-btn btn-clear-all">C</div>
+            <div className="calculator-btn btn-save">MR</div>
+            <div className="calculator-btn btn-operation">÷</div>
+            <div className="calculator-btn btn-digit">7</div>
+            <div className="calculator-btn btn-digit">8</div>
+            <div className="calculator-btn btn-digit">9</div>
+            <div className="calculator-btn btn-operation">*</div>
+            <div className="calculator-btn btn-digit">4</div>
+            <div className="calculator-btn btn-digit">5</div>
+            <div className="calculator-btn btn-digit">6</div>
+            <div className="calculator-btn btn-operation">-</div>
+            <div className="calculator-btn btn-digit">1</div>
+            <div className="calculator-btn btn-digit">2</div>
+            <div className="calculator-btn btn-digit">3</div>
+            <div className="calculator-btn btn-operation">+</div>
+            <div className="calculator-btn btn-operation">^</div>
+            <div className="calculator-btn btn-digit">0</div>
+            <div className="calculator-btn btn-digit">.</div>
+            <div className="calculator-btn btn-result">=</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
